4a5ca50ba7deff8e3387ca1ea8644e1cb6cc35e5
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-06-26 00:31:30 +0500

    adding whatsaap icon in layout

--- app/(landing)/page.js
+++ app/(landing)/page.js
@@ -9,7 +9,6 @@ export default function Home() {
   return (
     <>
     <Hero />
-    <WhatsaapIcon />
     <Categories />
     <Deals />
     <BulkOrder />
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/app-shell/Navbar";
 import Footer from "@/components/app-shell/Footer";
+import WhatsappIcon from "@/components/app-shell/WhatsaapIcon";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -171,6 +172,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         <Navbar />
         {children}
+        <WhatsappIcon />
         <Footer />
       </body>
     </html>
