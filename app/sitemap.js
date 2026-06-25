762e8dd9b595c7873cfe094bbc4e3d4719244a23
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-06-26 01:36:19 +0500

    fixing bug in sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,13 +1,21 @@
 import { CATEGORY_SLUGS } from "../data/categories";
 
+const BASE_URL = "https://valuemax.com.pk";
+
 export default async function sitemap() {
   const pages = [
-    { url: "/", lastModified: new Date() },
-    { url: "/policy", lastModified: new Date() },
+    {
+      url: `${BASE_URL}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${BASE_URL}/policy`,
+      lastModified: new Date(),
+    },
   ];
 
   const productPages = CATEGORY_SLUGS.map((slug) => ({
-    url: `/products/${slug}`,
+    url: `${BASE_URL}/products/${slug}`,
     lastModified: new Date(),
   }));
 
