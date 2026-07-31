8f6d14ef675963bb8e71f69ba126c8d03ccc5364
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-08-01 02:37:02 +0500

    use link in insted of router of aply job btn

--- components/app-shell/Navbar.js
+++ components/app-shell/Navbar.js
@@ -168,10 +168,11 @@ export default function Navbar() {
             </div>
 
             {/* Sign In — after search bar */}
-            <button className="border-2 border-[#b60a01] text-[#E8001C] hover:bg-[#b60a01] hover:text-white hover:-translate-y-px bg-transparent text-[13px] font-semibold px-4 py-[7px] rounded-lg cursor-pointer transition-all duration-200 whitespace-nowrap shrink-0"
-            onClick={()=>{router.push("/career")}}>
+            <Link href="/career">
+            <button className="border-2 border-[#b60a01] text-[#E8001C] hover:bg-[#b60a01] hover:text-white hover:-translate-y-px bg-transparent text-[13px] font-semibold px-4 py-[7px] rounded-lg cursor-pointer transition-all duration-200 whitespace-nowrap shrink-0">
               Apply For Jobs
             </button>
+            </Link>
           </div>
         </div>
 
@@ -311,11 +312,11 @@ export default function Navbar() {
                 )}
               </Link>
             ))}
-
-            <button className="mt-2 border-2 border-[#b60a01] text-[#E8001C] hover:bg-[#E8001C] hover:text-white bg-transparent text-sm font-semibold px-4 py-2 rounded-lg cursor-pointer transition-all duration-200"
-            onClick={()=>{router.push("/career")}}>
+            <Link href="/career">
+           <button className="mt-2 border-2 border-[#b60a01] text-[#E8001C] hover:bg-[#E8001C] hover:text-white bg-transparent text-sm font-semibold px-4 py-2 rounded-lg cursor-pointer transition-all duration-200">
             Apply For Jobs
             </button>
+            </Link>
           </div>
         </div>
       </nav>
